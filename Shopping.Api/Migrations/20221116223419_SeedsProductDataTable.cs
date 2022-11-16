2a3706d18cf52e4afffd488827801ba6b619a7e4
Luiz Gustavo Banin de Sena <luiz.sena@voeazul.com.br>
2022-11-16 19:42:12 -0300

    create initials values

---
+++ Shopping.Api/Migrations/20221116223419_SeedsProductDataTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping.Api.Migrations
+{
+    public partial class SeedsProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
