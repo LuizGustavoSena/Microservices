fc6f1831318d365aec426cd30b1c85f577ea4b5f
Luiz Gustavo Sena <luizgbs1@gmail.com>
2023-05-03 21:09:15 -0300

    feat: adjust models

--- Shopping.Order/Models/OrderDetail.cs
+++ Shopping.Order/Models/OrderDetail.cs
@@ -6,14 +6,20 @@ namespace Shopping.Order.Models
     [Table("order_detail")]
     public class OrderDetail : BaseEntity
     {
-        public long CardHeaderId { get; set; }
+        public long OrderHeaderId { get; set; }
 
-        [ForeignKey("cart_header_id")]
-        public virtual CartHeader CartHeader { get; set; }
-
+        [ForeignKey("order_header_id")]
+        public virtual OrderHeader OrderHeader { get; set; }
+        [Column("product_id")]
         public long ProductId { get; set; }
-        [ForeignKey("product_id")]
-        public virtual Product Product { get; set; }
+        [Column("count")]
+
         public int Count { get; set; }
+        [Column("product_name")]
+
+        public string ProductName { get; set; }
+        [Column("price")]
+
+        public decimal Price { get; set; }
     }
 }
--- Shopping.Order/Models/OrderHeader.cs
+++ Shopping.Order/Models/OrderHeader.cs
@@ -11,17 +11,32 @@ namespace Shopping.Order.Models
 
         [Column("coupon_code")]
         public string CouponCode { get; set; }
+        [Column("purchase_amount")]
         public decimal PurchaseAmount { get; set; }
+        [Column("discount_amout")]
         public decimal DiscountAmount { get; set; }
+        [Column("first_name")]
         public string FirstName { get; set; }
+        [Column("last_name")]
         public string LastName { get; set; }
+        [Column("purchase_date")]
         public DateTime DateTime { get; set; }
+        [Column("order_date")]
+        public DateTime OrderTime { get; set; }
+        [Column("phone")]
         public string Phone { get; set; }
+        [Column("email")]
         public string Email { get; set; }
+        [Column("card_number")]
         public string CardNumber { get; set; }
+        [Column("cvv")]
         public string CVV { get; set; }
+        [Column("expiry_month_year")]
         public string ExpiryMonthYear { get; set; }
+        [Column("cart_total_itens")]
         public int CartTotalItens { get; set; }
         public IEnumerable<OrderDetail> OrderDetails { get; set; }
+        [Column("payment_status")]
+        public bool PaymentStatus { get; set; }
     }
 }
