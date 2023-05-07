c4124d98bd50a68aa68ee2e065330aabb4538537
Luiz Gustavo Sena <luizgbs1@gmail.com>
2023-05-07 00:18:41 -0300

    feat: add VO coupon

---
+++ Shopping.Car/Data/ValueObject/CouponVO.cs
@@ -0,0 +1,9 @@
+namespace Shopping.Car.Data.ValueObjects
+{
+    public class CouponVO
+    {
+        public long Id { get; set; }
+        public string CouponCode { get; set; }
+        public decimal DiscountAmount { get; set; }
+    }
+}
