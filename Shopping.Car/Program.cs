ca7903f7c2abea3028958f90424c9fc946039c4f
Luiz Gustavo Sena <luizgbs1@gmail.com>
2023-05-07 00:19:21 -0300

    chore: add config to httpClient coupon

--- Shopping.Car/Program.cs
+++ Shopping.Car/Program.cs
@@ -18,9 +18,16 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<ICouponRepository, CouponRepository>();
 builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttpClient<ICouponRepository, CouponRepository>(s =>
+    s.BaseAddress = new Uri(
+        builder.Configuration["ServiceUrls:CouponApi"]
+    )
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
